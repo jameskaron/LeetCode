94b5330b755ac77e02e7ea4ae25df1947c900604
James JJ Liang <james.jj.liang@pccw.com>
2021-01-02 13:59:52 +0800

    1

--- 算法指导/1. 两数之和.py
+++ 算法指导/1. 两数之和.py
@@ -2,8 +2,6 @@
 #
 # 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
 #
-#  
-#
 # 示例:
 #
 # 给定 nums = [2, 7, 11, 15], target = 9
@@ -11,9 +9,32 @@
 # 因为 nums[0] + nums[1] = 2 + 7 = 9
 # 所以返回 [0, 1]
 #
+#方法一：暴力枚举
+
+#方法二：查表法
+# 思路及算法
+#
+# 注意到方法一的时间复杂度较高的原因是寻找 target - x 的时间复杂度过高。因此，我们需要一种更优秀的方法，能够快速寻找数组中是否存在目标元素。如果存在，我们需要找出它的索引。
+#
+# 使用哈希表，可以将寻找 target - x 的时间复杂度降低到从
+# O(N) 降低到 O(1)。
+#
+# 这样我们创建一个哈希表，对于每一个 x，我们首先查询哈希表中是否存在 target - x，然后将 x 插入到哈希表中，即可保证不会让 x 和自己匹配。
+
 from typing import List
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # TODO
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target-num], i]
+            hashtable[nums[i]] = i
+        return []
+
+
+if __name__ == '__main__':
+    s = Solution()
+    num1 = [2, 7, 11, 15]
+    assert s.twoSum(num1, target=9) == [0, 1]
